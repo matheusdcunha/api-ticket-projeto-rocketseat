033aad6acddd5ea3a5474ee36d7dff3aff34c250
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-02-16 23:52:12 -0300

    feat: Adiciona função de obter os parâmetros da query

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,17 +1,20 @@
 import { routes } from "../routes/index.js";
-
 import { Database } from "../database/database.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
 
 const database = new Database();
 
 export function routeHandler(request, response){
   const route = routes.find((route)=>{
-    console.log(route.path)
-    console.log(request.url)
     return route.method === request.method && route.path.test(request.url)
   });
 
   if(route){
+    const routeParams = request.url.match(route.path);
+    const { query } = routeParams.groups;
+
+    request.query = query ? extractQueryParams(query) : {};
+
     return route.controller({request, response, database});
   }
 
